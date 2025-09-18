54acb15c01ff13941a2c998ee5659dd6187441bf
Micael Contin Pucci <micaelpucci06@gamil.com>
2025-09-18 09:31:31 -0300

    Aula JS

---
+++ pratica.js
@@ -0,0 +1,5 @@
+export function converterParaBinario(numeroDecimal){
+    const numeroBinario = numeroDecimal.toString(2)
+    return numeroBinario
+}
+console.log(converterParaBinario(10))
---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { converterParaBinario } from './pratica.js';
+
+const app = express();
+const port = 3000;
+
+function somar(a,b,callback){
+    const resultado = a + b
+    callback(resultado)
+}
+
+app.get('/',(req,res) => {
+    res.send(somar(10,20,(resultado)=>{console.log(resultado)}))
+})
+
+app.get('/erro',(req,res) => {
+    try{
+        throw new Error('deu erro')
+    }catch(erro){
+        console.log(erro)
+}
+    res.send('endpoint com erro')
+})
+
+app.listen(port,() => {
+    console.log('http//localhost:'+port)
+})
+
+console.log(converterParaBinario(10))
